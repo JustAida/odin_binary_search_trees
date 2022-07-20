8ce97471783befcd2ab736db9e83aea90e95629b
JustAida <suradet40@gmail.com>
2022-07-20 19:03:35 +0700

    Add pseudocode

---
+++ lib/binary_search_trees.rb
@@ -0,0 +1,12 @@
+# Create a "Node" class that has the following attributes:
+#   "data" for the data it stores
+#   "left" for its left children
+#   "right" for its right children
+
+# Create a "Tree" class that accepts array when initialized.
+# The attribute for "Tree" class is:
+#   "root" which will return the value of #build_tree
+# Create a #build_tree method which takes an array then sort it, remove duplicates
+#   and turns it into a balanced binary tree. It returns level-0 root node.
+# Create an #insert and #delete method.
+# Create a #find method. It accepts a value and returns the node of that given value.
